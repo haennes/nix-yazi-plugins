c63c5cb937e728ea82843795dbe891fd1d83b987
haennes <hannes.hofmuth@gmail.com>
2024-12-24 23:58:36 +0100

    fg change to new

---
+++ plugins/fg/hm-module.nix
@@ -0,0 +1,25 @@
+{
+  options = { mkKeyOption, ... }:
+    { lib, ... }: {
+      keys = {
+        fzf_find_name = mkKeyOption {
+          on = [ "f" "f" ];
+          run = "plugin fg --args='fzf'";
+          desc = "find file by filename";
+        };
+        fzf_find_content = mkKeyOption {
+          on = [ "f" "g" ];
+          run = "plugin fg";
+          desc = "find file by content (fuzzy match)";
+        };
+        rg_find_content = mkKeyOption {
+          on = [ "f" "G" ];
+          run = "plugin fg --args='rg'";
+          desc = "find file by content (exact match)";
+        };
+      };
+    };
+  config = { cfg, setKeys, ... }:
+    { config, lib, ... }:
+    { } // (setKeys cfg.keys);
+}
