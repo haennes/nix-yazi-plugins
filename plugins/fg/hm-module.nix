beafca86841bd8f4449f0b2f74c702a11d888945
haennes <hannes.hofmuth@gmail.com>
2024-12-25 16:50:57 +0100

    fg fmt

--- plugins/fg/hm-module.nix
+++ plugins/fg/hm-module.nix
@@ -1,25 +1,41 @@
 {
-  options = { mkKeyOption, mkRuntimeDeps, ... }:
-    { pkgs, lib, ... }: {
+  options =
+    { mkKeyOption, mkRuntimeDeps, ... }:
+    { pkgs, lib, ... }:
+    {
       keys = {
         fzf_find_name = mkKeyOption {
-          on = [ "f" "f" ];
+          on = [
+            "f"
+            "f"
+          ];
           run = "plugin fg --args='fzf'";
           desc = "find file by filename";
         };
         fzf_find_content = mkKeyOption {
-          on = [ "f" "g" ];
+          on = [
+            "f"
+            "g"
+          ];
           run = "plugin fg";
           desc = "find file by content (fuzzy match)";
         };
         rg_find_content = mkKeyOption {
-          on = [ "f" "G" ];
+          on = [
+            "f"
+            "G"
+          ];
           run = "plugin fg --args='rg'";
           desc = "find file by content (exact match)";
         };
       };
-      runtimeDeps =
-        mkRuntimeDeps { pkgs = with pkgs; [ bat fzf ripgrep-all ]; };
+      runtimeDeps = mkRuntimeDeps {
+        pkgs = with pkgs; [
+          bat
+          fzf
+          ripgrep-all
+        ];
+      };
     };
   config = { cfg, setKeys, ... }: { pkgs, ... }: { } // (setKeys cfg.keys);
 }
